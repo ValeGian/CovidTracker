098b2a688c3031cee25708f9506d672d28788e65
ValeGian <46gianninivalerio@gmail.com>
2020-12-27 21:27:22 +0100

    Nation Node esempio

--- CovidTracker/Beans/src/main/java/it/unipi/dii/inginf/dsmt/covidtracker/ejbs/NationConsumerBean.java
+++ CovidTracker/Beans/src/main/java/it/unipi/dii/inginf/dsmt/covidtracker/ejbs/NationConsumerBean.java
@@ -1,13 +1,20 @@
 package it.unipi.dii.inginf.dsmt.covidtracker.ejbs;
 
+import it.unipi.dii.inginf.dsmt.covidtracker.communication.CommunicationMessage;
+import it.unipi.dii.inginf.dsmt.covidtracker.intfs.NationConsumer;
+import javafx.util.Pair;
+
 import javax.ejb.Stateful;
 
 @Stateful(name = "NationConsumerEJB")
-public class NationConsumerBean {
+public class NationConsumerBean implements NationConsumer {
 
     public NationConsumerBean() {
 
     }
 
-
+    @Override
+    public Pair<String, CommunicationMessage> handleConnectionRequest(CommunicationMessage cMsg) {
+        return null;
+    }
 }
--- CovidTracker/Nation/src/main/java/it/unipi/dii/inginf/dsmt/covidtracker/nation/NationNode.java
+++ CovidTracker/Nation/src/main/java/it/unipi/dii/inginf/dsmt/covidtracker/nation/NationNode.java
@@ -1,45 +1,46 @@
 package it.unipi.dii.inginf.dsmt.covidtracker.nation;
 
+import it.unipi.dii.inginf.dsmt.covidtracker.communication.CommunicationMessage;
 import it.unipi.dii.inginf.dsmt.covidtracker.intfs.HierarchyConnectionsRetriever;
 import it.unipi.dii.inginf.dsmt.covidtracker.intfs.NationConsumer;
 import it.unipi.dii.inginf.dsmt.covidtracker.intfs.Producer;
+import javafx.util.Pair;
 import org.json.simple.parser.ParseException;
 
 import javax.ejb.EJB;
-import javax.jms.Message;
-import javax.jms.MessageListener;
-import javax.jms.Queue;
-import javax.jms.QueueConnectionFactory;
+import javax.jms.*;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import java.io.IOException;
+import java.util.List;
 
 public class NationNode implements MessageListener {
 
     private final static NationNode instance = new NationNode();
 
     final static String QC_FACTORY_NAME = "jms/__defaultConnectionFactory";
-    final static String nodeName = "nation";
+    final static String myName = "nation";
+    static String myDestinationName;
+    static List<String> myChildrenDestinationNames;
 
     @EJB static Producer myProducer;
     @EJB static NationConsumer myConsumer;
     @EJB public static HierarchyConnectionsRetriever myHierarchyConnectionsRetriever;
 
     private NationNode() {
-
+        try {
+            myDestinationName = myHierarchyConnectionsRetriever.getMyDestinationName(myName);
+            myChildrenDestinationNames = myHierarchyConnectionsRetriever.getChildrenDestinationName(myName);
+        } catch (final IOException|ParseException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static NationNode getInstance() { return instance; }
 
     public static void main(String[] args) {
-        try {
-            setMessageListener(myHierarchyConnectionsRetriever.getMyDestinationName(nodeName));
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
+        setMessageListener(myDestinationName);
     }
 
     static void setMessageListener(final String QUEUE_NAME) {
@@ -49,14 +50,51 @@ public class NationNode implements MessageListener {
             QueueConnectionFactory qcf = (QueueConnectionFactory)ic.lookup(QC_FACTORY_NAME);
             qcf.createContext().createConsumer(myQueue).setMessageListener(instance);
         }
-        catch (NamingException e) {
+        catch (final NamingException e) {
             System.err.println(e.getMessage());
             e.printStackTrace();
         }
     }
 
     @Override
-    public void onMessage(Message message) {
+    public void onMessage(Message msg) {
+        if (msg instanceof ObjectMessage) {
+            try {
+                CommunicationMessage cMsg = (CommunicationMessage) ((ObjectMessage) msg).getObject();
+                Pair<String, CommunicationMessage> messageToSend;
+                switch (cMsg.getMessageType()) {
+                    case NO_ACTION_REQUEST:
+                        break;
 
+                    case CONNECTION_REQUEST:
+                        messageToSend = myConsumer.handleConnectionRequest(cMsg);
+                        if(messageToSend != null)
+                            myProducer.enqueue(messageToSend.getKey(), messageToSend.getValue());
+                        break;
+
+                    case AGGREGATION_REQUEST:
+                        messageToSend = myConsumer.handleAggregationRequest(cMsg);
+                        if(!messageToSend.getKey().equals("mySelf"))
+                            myProducer.enqueue(messageToSend.getKey(), messageToSend.getValue());
+                        else
+                            handleAggregation(cMsg);
+                        break;
+
+                    case DAILY_REPORT:
+                        messageToSend = myConsumer.handleDailyReport(cMsg);
+                        if(messageToSend != null) {
+                            myProducer.enqueue(messageToSend.getKey(), messageToSend.getValue());
+                            addDailyReport(new Gson().fromJson(messageToSend.getValue().getMessageBody(), DailyReport.class));
+                        }
+                        break;
+
+                    default:
+                        break;
+
+                }
+            } catch (final JMSException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
