bbb4e6a558cb698ba80cb2f607b6b15bbd9dff30
ValeGian <46gianninivalerio@gmail.com>
2020-12-27 15:11:08 +0100

    Hierarchy Interface

---
+++ CovidTracker/Nation/src/main/java/it/unipi/dii/inginf/dsmt/covidtracker/ejbs/HierarchyConnectionsRetrieverBean.java
@@ -0,0 +1,28 @@
+package it.unipi.dii.inginf.dsmt.covidtracker.ejbs;
+
+import it.unipi.dii.inginf.dsmt.covidtracker.intfs.HierarchyConnectionsRetriever;
+
+import javax.ejb.Stateless;
+import java.util.List;
+
+@Stateless(name = "HierarchyConnectionsRetrieverEJB")
+public class HierarchyConnectionsRetrieverBean implements HierarchyConnectionsRetriever {
+
+    public HierarchyConnectionsRetrieverBean() {
+    }
+
+    @Override
+    public String getMyDestinationName(String nodeName) {
+        return null;
+    }
+
+    @Override
+    public String getParentDestinationName(String nodeName) {
+        return null;
+    }
+
+    @Override
+    public List<String> getChildsDestinationName(String nodeName) {
+        return null;
+    }
+}
