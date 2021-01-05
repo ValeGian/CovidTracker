fb49ee47daf0f361b189aeaf3be44a9bdaacb9e3
lorenzo <l.bianchi45@studenti.unipi.it>
2021-01-05 16:21:24 +0100

    Aggiunta gestione topic lato Region

--- CovidTracker/Region/src/main/java/it/unipi/dii/inginf/dsmt/covidtracker/ejbs/RegionConsumerBean.java
+++ CovidTracker/Region/src/main/java/it/unipi/dii/inginf/dsmt/covidtracker/ejbs/RegionConsumerBean.java
@@ -22,7 +22,6 @@ public class RegionConsumerBean implements RegionConsumer {
     String myName;
     String myParent;
     CommunicationMessage myCommunicationMessage;
-    Map<AggregationRequest, String> aggregationToAnswer = new HashMap<AggregationRequest, String>();
 
     @Override
     public void initializeParameters(String myName, String parent) {
@@ -47,27 +46,11 @@ public class RegionConsumerBean implements RegionConsumer {
             myCommunicationMessage.setMessageType(MessageType.AGGREGATION_RESPONSE);
             return new Pair<>(cMsg.getSenderName(), myCommunicationMessage);
         }
-        else { //altrimenti la salvo nelle aggregazioni pendenti e la inoltro alla mia zona
-            aggregationToAnswer.put(aggregationRequest, cMsg.getSenderName());
+        else { //altrimenti preparo un messaggio da inoltrare alla mia regione
             myCommunicationMessage.setMessageType(MessageType.AGGREGATION_REQUEST);
             myCommunicationMessage.setMessageBody(gson.toJson(aggregationRequest));
             return new Pair<>(myParent, myCommunicationMessage);
         }
     }
 
-    @Override
-    public Pair<String, CommunicationMessage> handleAggregationResponse(CommunicationMessage cMsg){
-        Gson gson = new Gson();
-        AggregationRequest aggregationRequest = gson.fromJson(cMsg.getMessageBody(), AggregationRequest.class);
-        myCommunicationMessage.setMessageType(MessageType.AGGREGATION_RESPONSE);
-        myCommunicationMessage.setMessageBody(gson.toJson(aggregationRequest));
-
-        //rimuovo la richiesta di aggregazione dalle aggragazioni pendenti
-        AggregationRequest key = aggregationRequest;
-        key.setResult(-1.0);
-        String regionWeb = aggregationToAnswer.remove(key);
-        if (regionWeb != null)
-            return new Pair<>(regionWeb, myCommunicationMessage);
-        return null;
-    }
 }
