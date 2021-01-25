cf3e8be6ed6fd1d5b6ee69f91e1b69f76cc46934
lorenzo <l.bianchi45@studenti.unipi.it>
2021-01-25 13:06:16 +0100

    Sistemato il bug che ci ha fatto smadonnare

--- CovidTracker/Interfaces/src/main/java/it/unipi/dii/inginf/dsmt/covidtracker/intfs/regionInterfaces/RegionNode.java
+++ CovidTracker/Interfaces/src/main/java/it/unipi/dii/inginf/dsmt/covidtracker/intfs/regionInterfaces/RegionNode.java
@@ -1,8 +1,6 @@
 package it.unipi.dii.inginf.dsmt.covidtracker.intfs.regionInterfaces;
 
-import javax.ejb.Remote;
-
-@Remote
 public interface RegionNode {
     String readReceivedMessages();
+    String get();
 }
