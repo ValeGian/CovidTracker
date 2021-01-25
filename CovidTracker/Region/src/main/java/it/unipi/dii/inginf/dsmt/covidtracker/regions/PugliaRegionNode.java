c0ed5069adf516cba82541be0f8e06cc72eddda8
lorenzo <l.bianchi45@studenti.unipi.it>
2021-01-25 14:38:22 +0100

    Messe classi marker

---
+++ CovidTracker/Region/src/main/java/it/unipi/dii/inginf/dsmt/covidtracker/regions/PugliaRegionNode.java
@@ -0,0 +1,32 @@
+package it.unipi.dii.inginf.dsmt.covidtracker.regions;
+
+import it.unipi.dii.inginf.dsmt.covidtracker.ejbs.GenericRegionNode;
+import it.unipi.dii.inginf.dsmt.covidtracker.intfs.regionInterfaces.RegionPuglia;
+import it.unipi.dii.inginf.dsmt.covidtracker.persistence.KVManagerImpl;
+import org.json.simple.parser.ParseException;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Stateful;
+import java.io.IOException;
+
+@Stateful(name = "PugliaRegionEJB")
+public class PugliaRegionNode extends GenericRegionNode implements RegionPuglia {
+    @PostConstruct
+    public void init(){
+        try {
+            myName = "puglia";
+            myDestinationName = myHierarchyConnectionsRetriever.getMyDestinationName(myName);
+            myAreaDestinationName = myHierarchyConnectionsRetriever.getParentDestinationName(myName);
+
+            myKVManager = new KVManagerImpl(myName);
+            myKVManager.deleteAllClientRequest();
+
+            myMessageHandler.initializeParameters(myName, myDestinationName, myAreaDestinationName);
+
+            setQueueConsumer(myDestinationName);
+            startReceivingLoop();
+        } catch (ParseException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ CovidTracker/Region/src/main/java/it/unipi/dii/inginf/dsmt/covidtracker/regions/ToscanaRegionNode.java
@@ -0,0 +1,32 @@
+package it.unipi.dii.inginf.dsmt.covidtracker.regions;
+
+import it.unipi.dii.inginf.dsmt.covidtracker.ejbs.GenericRegionNode;
+import it.unipi.dii.inginf.dsmt.covidtracker.intfs.regionInterfaces.RegionToscana;
+import it.unipi.dii.inginf.dsmt.covidtracker.persistence.KVManagerImpl;
+import org.json.simple.parser.ParseException;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Stateful;
+import java.io.IOException;
+
+@Stateful(name = "ToscanaRegionEJB")
+public class ToscanaRegionNode extends GenericRegionNode implements RegionToscana {
+    @PostConstruct
+    public void init(){
+        try {
+            myName = "toscana";
+            myDestinationName = myHierarchyConnectionsRetriever.getMyDestinationName(myName);
+            myAreaDestinationName = myHierarchyConnectionsRetriever.getParentDestinationName(myName);
+
+            myKVManager = new KVManagerImpl(myName);
+            myKVManager.deleteAllClientRequest();
+
+            myMessageHandler.initializeParameters(myName, myDestinationName, myAreaDestinationName);
+
+            setQueueConsumer(myDestinationName);
+            startReceivingLoop();
+        } catch (ParseException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
